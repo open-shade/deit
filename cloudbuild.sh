bccb365c1bcdda9c39cdd4f3a809bc7e082de6a5
Brandon Fan <brandonfan1256@gmail.com>
2022-07-08 14:03:15 -0400

    update deit cloud build

--- cloudbuild.sh
+++ cloudbuild.sh
@@ -1,9 +1,7 @@
 declare -a ROS_VERSIONS=( "foxy" "galactic" "humble" "rolling" )
 
 ORGANIZATION="facebook"
-declare -a MODEL_VERSIONS=( "deit-base-patch16-224", "deit-tiny-patch16-224", 
-"deit-base-distilled-patch16-224", "deit-small-patch16-224", 
-"deit-tiny-distilled-patch16-224", "deit-base-distilled-patch16-384", "deit-small-distilled-patch16-224" )
+declare -a MODEL_VERSIONS=( "deit-base-patch16-224" "deit-tiny-patch16-224" "deit-base-distilled-patch16-224" "deit-small-patch16-224" "deit-tiny-distilled-patch16-224" "deit-base-distilled-patch16-384" "deit-small-distilled-patch16-224" )
 
 for VERSION in "${ROS_VERSIONS[@]}"
 do
@@ -12,7 +10,7 @@ do
     ROS_VERSION="$VERSION"
     gcloud builds submit --config cloudbuild.yaml . --substitutions=_ROS_VERSION="$ROS_VERSION",_MODEL_VERSION="$MODEL_VERSION",_ORGANIZATION="$ORGANIZATION" --timeout=10000 &
     pids+=($!)
-    echo Dispached "$MODEL_VERSION" on ROS "$ROS_VERSION"
+    echo Dispatched "$MODEL_VERSION" on ROS "$ROS_VERSION"
   done
 done
 
